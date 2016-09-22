ed5b412db9bde6e47bb39872b5623c6cd4243cdb
jaarboledam <jaarboledam@gmail.com>
2016-09-21 23:58:36 -0500

    Se crea formulario de login

---
+++ users/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.forms import ModelForm, PasswordInput
+
+
+class LoginForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+        widgets = {
+            'password': PasswordInput()
+        }
+
+
+class RegisterForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'password']
